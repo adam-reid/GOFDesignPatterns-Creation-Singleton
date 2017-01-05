bde98eac8873262f310d205c2ca817b204ec2ecd
Adam Reid <adameastcoast@gmail.com>
2017-01-04 23:13:28 -0500

    Test logic; ignore javadocs

--- src/main/java/com/training/singleton/Singleton.java
+++ src/main/java/com/training/singleton/Singleton.java
@@ -1,7 +1,12 @@
 package com.training.singleton;
 
 public class Singleton {
+
+	private static Singleton instance = null;
+	private static ScrabbleTiles scrabbleTiles = new ScrabbleTiles();
+
 	public static void main(String... args) {
-		System.out.println("Test test.");
+		System.out.println(scrabbleTiles.toString());
+		System.out.println(scrabbleTiles.countOfTiles());
 	}
 }
